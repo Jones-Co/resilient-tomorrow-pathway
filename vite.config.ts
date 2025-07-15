09eaf87e167e0aa8b7e913f3facfaf80a25631da
Mike Jones <mejones73@protonmail.com>
2025-07-15 16:00:40 -0700

    Update vite.config.ts
    Updated with "base: '/'" for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // ✅ IMPORTANT for Vercel + custom domain
+
   server: {
     host: "::",
     port: 8080,
